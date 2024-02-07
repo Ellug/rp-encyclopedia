b80075704786e6f62095575679166c28a9eb3897
Ellug <auron975@nate.com>
2024-02-08 00:41:16 +0900

    editting

--- src/components/DetailModal.jsx
+++ src/components/DetailModal.jsx
@@ -1,16 +1,29 @@
 // src/components/DetailModal.jsx
 import React, { useState, useEffect } from 'react';
-import { doc, updateDoc } from 'firebase/firestore';
+import { deleteDoc, setDoc, doc } from 'firebase/firestore';
 import { getFirestore } from 'firebase/firestore';
-import '../styles/DetailModal.css'
+import '../styles/DetailModal.css';
 
 const DetailModal = ({ character, onClose, onDelete, nowYear }) => {
-  const [editCharacter, setEditCharacter] = useState(character);
   const [isEditing, setIsEditing] = useState(false);
   const db = getFirestore();
+  const originalDocId = `${character.name} ${character.family}`;
+
+  const createDefaultCharacter = (characterData) => {
+    const fields = 
+      ['birth', 'name', 'family', 'title', 'gender', 'unit', 'party', 'weapon', 'hobby', 'talent', 
+      'body', 'country', 'detail'];
+    const defaultCharacter = {};
+    fields.forEach(field => {
+      defaultCharacter[field] = characterData[field] || ''; // 존재하지 않는 필드는 빈 문자열로 설정
+    });
+    return defaultCharacter;
+  };
+
+  const [editCharacter, setEditCharacter] = useState(createDefaultCharacter(character));
 
   useEffect(() => {
-    setEditCharacter(character);
+    setEditCharacter(createDefaultCharacter(character));
   }, [character]);
 
   const handleEditChange = (e) => {
@@ -18,12 +31,23 @@ const DetailModal = ({ character, onClose, onDelete, nowYear }) => {
   };
 
   const saveEdit = async () => {
+    const newDocId = `${editCharacter.name} ${editCharacter.family}`;
+    const newDocRef = doc(db, "char", newDocId);
+  
     try {
-      await updateDoc(doc(db, "char", `${editCharacter.name} ${editCharacter.family}`), editCharacter);
+      // 새로운 문서 식별자로 문서를 생성
+      await setDoc(newDocRef, editCharacter);
+  
+      // 기존 문서 식별자와 새 문서 식별자가 다르면 기존 문서 삭제
+      if (originalDocId !== newDocId) {
+        const originalDocRef = doc(db, "char", originalDocId);
+        await deleteDoc(originalDocRef);
+      }
+  
       setIsEditing(false);
       onClose(); // 모달 닫기
     } catch (error) {
-      console.error("Error updating document: ", error);
+      console.error("Error saving document: ", error);
     }
   };
 
@@ -32,10 +56,7 @@ const DetailModal = ({ character, onClose, onDelete, nowYear }) => {
     setIsEditing(false);
   };
 
-  const calculateAge = (birthYear) => {
-    const currentYear = nowYear; // 현재 연도를 가져옵니다.
-    return currentYear - birthYear;
-  };
+  const calculateAge = (birthYear) => nowYear - birthYear;
 
   return (
     <div className="modal-background" onClick={onClose}>
@@ -69,7 +90,6 @@ const DetailModal = ({ character, onClose, onDelete, nowYear }) => {
         ) : (
           <>
             {/* 인풋 필드들 */}
-            <input type="text" name="name" value={editCharacter.name} onChange={handleEditChange} />
             <input type="text" name="birth" value={editCharacter.birth} onChange={handleEditChange} />
             <input type="text" name="name" value={editCharacter.name} onChange={handleEditChange} />
             <input type="text" name="family" value={editCharacter.family} onChange={handleEditChange} />
@@ -77,6 +97,7 @@ const DetailModal = ({ character, onClose, onDelete, nowYear }) => {
             <input type="text" name="gender" value={editCharacter.gender} onChange={handleEditChange} />
             <input type="text" name="unit" value={editCharacter.unit} onChange={handleEditChange} />
             <input type="text" name="party" value={editCharacter.party} onChange={handleEditChange} />
+            <input type="text" name="personality" value={editCharacter.personality} onChange={handleEditChange} />
             <input type="text" name="weapon" value={editCharacter.weapon} onChange={handleEditChange} />
             <input type="text" name="hobby" value={editCharacter.hobby} onChange={handleEditChange} />
             <input type="text" name="talent" value={editCharacter.talent} onChange={handleEditChange} />           
--- src/pages/Character.jsx
+++ src/pages/Character.jsx
@@ -81,25 +81,49 @@ const Character = () => {
         <input type="number" value={currentYear} onChange={handleYearChange} placeholder="현재 연도" />
       </div>
 
-      <div style={{ height: '700px', width: '1750px', overflowY: 'scroll', margin: '10px auto', padding: '10px', border: '1px solid white', position: 'relative' }}>
+      <div style={{ height: '700px', width: '1800px', overflowY: 'scroll', margin: '10px auto', padding: '10px', border: '1px solid white', position: 'relative' }}>
       {characters.map((character) => (
           <div className='indexs' key={character.id}>
               <div className='infos'>
                 <div className='profile'>
-                  <div className='info birth'>{character.birth}</div>
-                  <div className='info name'>{character.name}</div>
-                  <div className='info fam'>{character.family}</div>
-                  <div className='info title'>{character.title}</div>
-                  <div className='info age'>{calculateAge(character.birth)}세</div>
-                  <div className='info gen'>{character.gender}</div>
-                  <div className='info unit'>{character.unit}</div>
-                  <div className='info party'>{character.party}</div>
-                  <div className='info personality'>{character.personality}</div>
-                  <div className='info weapon'>{character.weapon}</div>
-                  <div className='info hobby'>{character.hobby}</div>
-                  <div className='info talent'>{character.talent}</div>
-                  <div className='info body'>{character.body}</div>
-                  <div className='info country'>{character.country}</div>
+                  <div className='info birth'>
+                    {character.birth}</div>
+                    <div className='info name'>
+                      {character.name?.substring(0, 6)}{character.name?.length > 6 ? '...' : ''}
+                    </div>
+                    <div className='info fam'>
+                      {character.family?.substring(0, 7)}{character.family?.length > 7 ? '...' : ''}
+                    </div>
+                    <div className='info title'>
+                      {character.title?.substring(0, 9)}{character.title?.length > 9 ? '...' : ''}
+                    </div>
+                  <div className='info age'>
+                    {calculateAge(character.birth)}세</div>
+                  <div className='info gen'>
+                    {character.gender}</div>
+                  <div className='info unit'>
+                    {character.unit?.substring(0, 5)}{character.unit?.length > 5 ? '...' : ''}
+                    </div>
+                  <div className='info party'>
+                    {character.party?.substring(0, 10)}{character.party?.length > 10 ? '...' : ''}
+                  </div>
+                  <div className='info personality'>
+                    {character.personality}</div>
+                  <div className='info weapon'>
+                    {character.weapon?.substring(0, 11)}{character.weapon?.length > 11 ? '...' : ''}
+                  </div>
+                  <div className='info hobby'>
+                    {character.hobby?.substring(0, 6)}{character.hobby?.length > 6 ? '...' : ''}
+                  </div>
+                  <div className='info talent'>
+                    {character.talent?.substring(0, 6)}{character.talent?.length > 6 ? '...' : ''}
+                  </div>
+                  <div className='info body'>
+                    {character.body?.substring(0, 14)}{character.body?.length > 14 ? '...' : ''}
+                  </div>
+                  <div className='info country'>
+                    {character.country?.substring(0, 8)}{character.country?.length > 8 ? '...' : ''}
+                  </div>
                 </div>
                 <div className='btn-container'>
                   <button onClick={() => openModal(character)}>상세 정보</button>
@@ -112,19 +136,19 @@ const Character = () => {
        {/* 입력 필드 추가 */}
        <div className='add-inputs'>
         <div className='add-profile'>
-          <input type="text" name="birth" value={newCharacter.birth} onChange={handleNewCharacterChange } placeholder="Birth" />
-          <input type="text" name="name" value={newCharacter.name} onChange={handleNewCharacterChange } placeholder="Name" />
-          <input type="text" name="family" value={newCharacter.family} onChange={handleNewCharacterChange } placeholder="Family" />
-          <input type="text" name="title" value={newCharacter.title} onChange={handleNewCharacterChange } placeholder="title" />
-          <input type="text" name="gender" value={newCharacter.gender} onChange={handleNewCharacterChange } placeholder="Gender" />
-          <input type="text" name="unit" value={newCharacter.unit} onChange={handleNewCharacterChange } placeholder="Unit" />
-          <input type="text" name="party" value={newCharacter.party} onChange={handleNewCharacterChange } placeholder="Party" />
-          <input type="text" name="personality" value={newCharacter.personality} onChange={handleNewCharacterChange } placeholder="personality" />
-          <input type="text" name="weapon" value={newCharacter.weapon} onChange={handleNewCharacterChange } placeholder="weapon" />
-          <input type="text" name="hobby" value={newCharacter.hobby} onChange={handleNewCharacterChange } placeholder="hobby" />
-          <input type="text" name="talent" value={newCharacter.talent} onChange={handleNewCharacterChange } placeholder="talent" />
-          <input type="text" name="body" value={newCharacter.body} onChange={handleNewCharacterChange } placeholder="body" />
-          <input type="text" name="country" value={newCharacter.country} onChange={handleNewCharacterChange } placeholder="country" />
+          <input type="number" name="birth" value={newCharacter.birth} onChange={handleNewCharacterChange } placeholder="Birth" autoComplete='off'/>
+          <input type="text" name="name" value={newCharacter.name} onChange={handleNewCharacterChange } placeholder="Name" autoComplete='off' />
+          <input type="text" name="family" value={newCharacter.family} onChange={handleNewCharacterChange } placeholder="Family" autoComplete='off' />
+          <input type="text" name="title" value={newCharacter.title} onChange={handleNewCharacterChange } placeholder="title" autoComplete='off' />
+          <input type="text" name="gender" value={newCharacter.gender} onChange={handleNewCharacterChange } placeholder="Gender" autoComplete='off' />
+          <input type="text" name="unit" value={newCharacter.unit} onChange={handleNewCharacterChange } placeholder="Unit" autoComplete='off' />
+          <input type="text" name="party" value={newCharacter.party} onChange={handleNewCharacterChange } placeholder="Party" autoComplete='off' />
+          <input type="text" name="personality" value={newCharacter.personality} onChange={handleNewCharacterChange } placeholder="personality" autoComplete='off' />
+          <input type="text" name="weapon" value={newCharacter.weapon} onChange={handleNewCharacterChange } placeholder="weapon" autoComplete='off' />
+          <input type="text" name="hobby" value={newCharacter.hobby} onChange={handleNewCharacterChange } placeholder="hobby" autoComplete='off' />
+          <input type="text" name="talent" value={newCharacter.talent} onChange={handleNewCharacterChange } placeholder="talent" autoComplete='off' />
+          <input type="text" name="body" value={newCharacter.body} onChange={handleNewCharacterChange } placeholder="body" autoComplete='off' />
+          <input type="text" name="country" value={newCharacter.country} onChange={handleNewCharacterChange } placeholder="country" autoComplete='off' />
         </div>
         <input className='add-detail' type="text" name="detail" value={newCharacter.detail} onChange={handleNewCharacterChange } placeholder="Detail" />
       </div>
